9206d3868a169d5dca85448a5578f2f6c8c8b1b7
Robson <robccorreia@gmail.com>
2024-05-01 17:32:01 -0300

    feat: add theme provider and styled types

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,19 @@
+'use client'
+
 import { PropsWithChildren } from 'react'
 
+import { ThemeProvider } from 'styled-components'
+
 import GlobalStyles from '@/styles/global'
+import theme from '@/styles/theme'
 
 export function Providers({ children }: PropsWithChildren) {
   return (
     <>
-      <GlobalStyles />
-      {children}
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        {children}
+      </ThemeProvider>
     </>
   )
 }
