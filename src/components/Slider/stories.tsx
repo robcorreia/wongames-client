6d5fb8b010c3dc8bbf715183e593f8b251f203ee
Robson <robccorreia@gmail.com>
2024-09-22 10:49:37 -0300

    feat: add slider base component

---
+++ src/components/Slider/stories.tsx
@@ -0,0 +1,62 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Slider from '.'
+import { Settings } from 'react-slick'
+import styled from 'styled-components'
+
+export default {
+  title: 'Slider',
+  component: Slider
+} as Meta
+
+const settings: Settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1
+}
+
+const Slide = styled.div`
+  background: gray;
+  width: 30rem;
+  padding: 10rem 0;
+  border: 0.1rem solid red;
+  text-align: center;
+  color: white;
+`
+
+export const Horizontal: StoryObj = () => {
+  return (
+    <Slider settings={settings}>
+      <Slide>1</Slide>
+      <Slide>2</Slide>
+      <Slide>3</Slide>
+      <Slide>4</Slide>
+      <Slide>5</Slide>
+      <Slide>6</Slide>
+      <Slide>7</Slide>
+    </Slider>
+  )
+}
+
+const verticalSettings: Settings = {
+  vertical: true,
+  verticalSwiping: true,
+  dots: true,
+  infinite: false,
+  slidesToShow: 1
+}
+
+export const Vertical: StoryObj = () => {
+  return (
+    <Slider settings={verticalSettings}>
+      <Slide>1</Slide>
+      <Slide>2</Slide>
+      <Slide>3</Slide>
+      <Slide>4</Slide>
+      <Slide>5</Slide>
+      <Slide>6</Slide>
+      <Slide>7</Slide>
+    </Slider>
+  )
+}
