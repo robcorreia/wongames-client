a51d8e2d99e7e651f4676868a4758a017a630c97
Robson <robccorreia@gmail.com>
2024-10-03 21:50:50 -0300

    feat: add the first static components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import Main from '@/components/Main'
+'use client'
+import Home from '@/templates/Home'
 
-export default function Home() {
-  return <Main />
+export default function Index() {
+  return <Home />
 }
