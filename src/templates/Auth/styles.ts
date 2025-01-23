e8bd9a198dccefed30a0d62b71d5363e9c03d165
Robson <robccorreia@gmail.com>
2025-01-23 20:20:08 -0300

    feat: add initial template content

--- src/templates/Auth/styles.ts
+++ src/templates/Auth/styles.ts
@@ -1,3 +1,13 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.main``
+
+export const BannerBlock = styled.div``
+
+export const SubTittle = styled.h3``
+
+export const Footer = styled.p`
+  text-align: center;
+`
+
+export const Content = styled.div``
--- src/templates/Auth/test.tsx
+++ src/templates/Auth/test.tsx
@@ -1,13 +1,21 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 
 import Auth from '.'
+import { renderWithTheme } from '../../utils/tests/helpers'
 
 describe('<Auth />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Auth />)
+  it('should render logos, title, children', () => {
+    renderWithTheme(<Auth title="Auth title">Children</Auth>)
 
-    expect(screen.getByRole('heading', { name: /Auth/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
+    expect(
+      screen.getByRole('heading', {
+        name: /all your favorite games in one place/i
+      })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /auth title/i })
+    ).toBeInTheDocument()
+    expect(screen.getByText(/children/i)).toBeInTheDocument()
   })
 })
