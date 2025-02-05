1afab35d703ceef13df96dc66534208ed4ad507a
Robson <robccorreia@gmail.com>
2025-02-05 19:50:25 -0300

    feat: add new size on heading

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -15,7 +15,7 @@ const Auth = ({ title, children }: AuthProps) => (
         <Logo id="banner" />
 
         <div>
-          <Heading>All your favorite games in one place</Heading>
+          <Heading size="huge">All your favorite games in one place</Heading>
 
           <S.Subtitle>
             <strong>WOW</strong> is the best and most complete gaming plataform.
